666488d2f09341c02128a3cbd60e623af4bba94b
Patrick Fang <patrick910105@gmail.com>
2022-07-21 00:35:22 +0800

    1 type

---
+++ 1 type.r
@@ -0,0 +1,81 @@
+class(2)
+class(2L)
+class(TRUE)
+class("Learning R the easy way")
+class(Sys.Date())
+class(Sys.time())
+my_num <- 2
+class(my_num)
+my_num <- 2.33
+class(my_num)
+my_num <- 2.0
+class(my_num)
+my_num <- 2
+class(my_num)
+my_int <- 2L
+class(my_int)
+my_int <- 2.0L
+class(my_int)
+my_int <- 2.33L
+class(my_int)
+class(TRUE)
+class(FALSE)
+class(T)
+class(F)
+class(TRUE)
+class(True)
+class(true)
+8 > 7
+8 < 7
+8 >= 7
+8 <= 7
+8 == 7
+8 != 7
+7 %in% c(8, 7)
+first_num <- 8
+second_num <- 7
+
+first_num + second_num
+first_num - second_num
+first_num * second_num
+first_num / second_num
+first_num^second_num
+first_num %% second_num
+first_int <- 8L
+second_int <- 7L
+
+ans <- first_int * second_int
+ans
+class(ans)
+ans <- first_int / second_int
+ans
+class(ans)
+my_num <- 8
+my_int <- 7L
+ans <- my_num + my_int
+class(ans)
+my_num <- 8
+my_int <- 7L
+my_logi <- TRUE
+
+ans <- my_num + my_int + my_logi
+ans
+class(ans)
+
+my_logi <- FALSE
+ans <- my_num + my_int + my_logi
+ans
+class(ans)
+my_num <- 0
+my_int <- 0L
+my_logi <- FALSE
+ 
+my_num == my_logi
+my_int == my_logi
+
+my_num <- 1
+my_int <- 1L
+my_logi <- TRUE
+ 
+my_num == my_logi
+my_int == my_logi
