315cad7e6b08e0350f1a50526c2a1e82fb465611
Patrick0105 <patrick910105@gmail.com>
2022-08-05 08:09:31 +0800

    adad

--- 1 type.r
+++ 1 type.r
@@ -1,81 +1,47 @@
+a <- 213
+b <- 's'
+a
+b
 class(2)
 class(2L)
 class(TRUE)
-class("Learning R the easy way")
+class('123')
 class(Sys.Date())
 class(Sys.time())
-my_num <- 2
-class(my_num)
-my_num <- 2.33
-class(my_num)
-my_num <- 2.0
-class(my_num)
-my_num <- 2
-class(my_num)
-my_int <- 2L
-class(my_int)
-my_int <- 2.0L
-class(my_int)
-my_int <- 2.33L
-class(my_int)
+class(a)
+class(2)
+class(2.0)
+class(2L)
+a <- 3.33L  
+a
+class(t)
+class(T)
 class(TRUE)
 class(FALSE)
-class(T)
 class(F)
-class(TRUE)
-class(True)
-class(true)
 8 > 7
-8 < 7
-8 >= 7
-8 <= 7
 8 == 7
+7 %in% c(8,6)
 8 != 7
-7 %in% c(8, 7)
-first_num <- 8
-second_num <- 7
-
-first_num + second_num
-first_num - second_num
-first_num * second_num
-first_num / second_num
-first_num^second_num
-first_num %% second_num
-first_int <- 8L
-second_int <- 7L
-
-ans <- first_int * second_int
+8.0 %% 7.0
+a <- 8
+b <- 7
+a + b
+a ^ b
+a %% b
+a / b
+ans <- a+b
 ans
-class(ans)
-ans <- first_int / second_int
-ans
-class(ans)
-my_num <- 8
-my_int <- 7L
-ans <- my_num + my_int
-class(ans)
-my_num <- 8
-my_int <- 7L
-my_logi <- TRUE
+#TRUE == 1 == 1L
+#FALSE == 0 == 0L
 
-ans <- my_num + my_int + my_logi
-ans
-class(ans)
-
-my_logi <- FALSE
-ans <- my_num + my_int + my_logi
-ans
-class(ans)
-my_num <- 0
-my_int <- 0L
-my_logi <- FALSE
- 
-my_num == my_logi
-my_int == my_logi
-
-my_num <- 1
-my_int <- 1L
-my_logi <- TRUE
- 
-my_num == my_logi
-my_int == my_logi
+#=====練習題
+#1. 將您的身高（公分）指派給 my_height；體重（公斤）指派給 my_weight
+my_height <- 186
+my_weight <- 60
+#2. 分別將 my_height 與 my_weight 輸出在命令列
+my_height
+my_weight
+#利用 my_height 與 my_weight 計算您的身體質量指數
+bmi <- my_weight/(my_height/100)^2
+bmi
